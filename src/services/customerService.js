dc37b17b75d038623ef55eec5a7dee640d69b8ca
Hamza Ahmed <Hamzaunaiz@gmail.com>
2024-05-30 18:09:52 +1000

    Fixed Environment Variable Issue

--- src/services/customerService.js
+++ src/services/customerService.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = process.env.BASE_URL || 'http://localhost:8080';
+const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:8080';
 
 const customerService = {
+    
     searchCustomers: async (search) => {
         const url = `${BASE_URL}/customers/search?query=${encodeURIComponent(search)}`; // Correct variable name used
         try {
--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.BASE_URL || 'http://localhost:8080';
+const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:8080';
 
 const orderService = {
     findByCustomerId: async (customer_id) => {
